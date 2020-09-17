523fd13a875ff5abad9e5a043d7a9a988ef2f544
Leslie G <lesliegonzalez@me.com>
2020-09-17 21:33:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,25 +5,20 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
-    self.class.all << self
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      self.new(student)
+    students_array.each do |student_hash|
+      Student.new(student_hash)
     end
   end
 
   def add_student_attributes(attributes_hash)
-    self.twitter = attributes_hash[:twitter]
-    self.linkedin = attributes_hash[:linkedin]
-    self.github = attributes_hash[:github]
-    self.blog = attributes_hash[:blog]
-    self.profile_quote = attributes_hash[:profile_quote]
-    self.bio = attributes_hash[:bio]
-    self.profile_url = attributes_hash[:profile_url]
+    attributes_hash.each
   end
 
   def self.all
